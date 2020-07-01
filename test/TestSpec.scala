9293c24db1984513a76fd9da2cc3ed3a0c446d9e
Kevin Laeufer <ekiwi@berkeley.edu>
2020-06-30 18:46:00 -0700

    test: example test

---
+++ test/TestSpec.scala
@@ -0,0 +1,14 @@
+// Copyright 2020 The Regents of the University of California
+// released under BSD 3-Clause License
+// author: Kevin Laeufer <laeufer@cs.berkeley.edu>
+
+import org.scalatest._
+
+class TestSpec extends FlatSpec {
+
+  it should "something" in {
+    println("test")
+  }
+
+}
+
