fba71bd7b2fbbf6c4b37f0900b8063fe47235cb5
Kevin Laeufer <ekiwi@berkeley.edu>
2020-06-30 18:50:34 -0700

    document some thoughts

--- src/maltese/Maltese.scala
+++ src/maltese/Maltese.scala
@@ -4,6 +4,8 @@
 
 package maltese
 
+import uclid.smt.Btor2
+
 object MalteseApp extends App {
   if(args.length < 1) {
     println(s"please provide the name of a btor file")
@@ -13,5 +15,21 @@ object MalteseApp extends App {
 }
 
 object Maltese {
-  def load(filename: String): Unit = ???
+  def load(filename: String): Unit = {
+    // load transition system from file
+    val sys = Btor2.load(filename)
+
+    // build model checking context
+
+    // do we start with an arbitrary state or a defined state?
+    // i.e. ignore init state?
+    // we need to find all the dependencies of each formula and encode them into indexes
+    // every signal gets a numeric (Int) index
+    // negative index means one step in the past
+    // Thus something like a := x + 1 where x is a state variable would have a negative dependency
+    // dependencies are tracked in order to know which cell(s) need to be evaluated before the current cell
+    // question: how do we deal with "dynamic" dependencies? e.g., mux(cond, a, b)
+    // if we know that cond is false, we only need to compute b, not a...
+
+  }
 }
