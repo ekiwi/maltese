a4b436114fd8075d377c409059df3de37d516bdb
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-08-18 16:30:53 -0700

    Simplifier: ite

--- src/maltese/smt/SMTSimplifier.scala
+++ src/maltese/smt/SMTSimplifier.scala
@@ -17,9 +17,39 @@ object SMTSimplifier {
     case BVExtend(e, 0, _) => e
     case BVSlice(e, hi, 0) if hi == e.width - 1 => e
     case BVNot(BVNot(e)) => e
-    case BVIte(condA, BVIte(condB, truB, falsB), falsA) if falsA == falsB => BVIte(and(condA, condB), truB, falsA)
+    case ite: BVIte => simplifyBVIte(ite)
     case op: BVOp => simplifyOp(op)
     case other => other
+  }
+
+  private val True = BVLiteral(1,1)
+  private val False = BVLiteral(0,1)
+
+  private def simplifyBVIte(i: BVIte): BVExpr = (i.cond, i.tru, i.fals) match {
+    // constant condition
+    case (True, tru, _) => tru
+    case (False, _, fals) => fals
+
+    // same result
+    case (_, tru, fals) if tru == fals => tru
+
+    // boolean result (all verified with sympy)
+    // simplify_logic(ITE(c, 1, 0)) = c
+    case (cond, True, False) => cond
+    // simplify_logic(ITE(c, 0, 1)) = ~c
+    case (cond, False, True) => BVNot(cond)
+    // simplify_logic(ITE(c, 1, b)) = b | c
+    case (cond, True, b) => or(cond, b)
+    // simplify_logic(ITE(c, 0, b)) = b & ~c
+    case (cond, False, b) => and(not(cond), b)
+    // simplify_logic(ITE(c, b, 1)) = b | ~c
+    case (cond, b, True) => or(not(cond), b)
+    // simplify_logic(ITE(c, b, 0)) = b & c
+    case (cond, b, False) => and(cond, b)
+
+    // nested ite
+    case (condA, BVIte(condB, truB, falsB), falsA) if falsA == falsB => BVIte(and(condA, condB), truB, falsA)
+    case _ => i
   }
 
   private def simplifyOp(expr: BVOp): BVExpr = expr match {
@@ -34,5 +64,7 @@ object SMTSimplifier {
     case other => other
   }
 
-  private def and(a: BVExpr, b: BVExpr): BVExpr = BVOp(Op.And, a, b)
+  private def and(a: BVExpr, b: BVExpr): BVOp = BVOp(Op.And, a, b)
+  private def or(a: BVExpr, b: BVExpr): BVOp = BVOp(Op.Or, a, b)
+  private def not(a: BVExpr): BVNot = BVNot(a)
 }
