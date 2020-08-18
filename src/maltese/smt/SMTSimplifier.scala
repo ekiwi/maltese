472b526c1ee9b1592bc6b3a474647230d8853b7d
Kevin Läufer <laeufer@cs.berkeley.edu>
2020-08-18 15:48:21 -0700

    move functionality to SMTSimplifier

--- src/maltese/smt/SMTSimplifier.scala
+++ src/maltese/smt/SMTSimplifier.scala
@@ -1,5 +1,37 @@
+// Copyright 2020 The Regents of the University of California
+// released under BSD 3-Clause License
+// author: Kevin Laeufer <laeufer@cs.berkeley.edu>
+
 package maltese.smt
 
-class SMTSimplifier {
+object SMTSimplifier {
 
+  def simplify(expr: SMTExpr): SMTExpr = SMTExprVisitor.map(simplifySingle)(expr)
+
+  private def simplifySingle(expr: SMTExpr): SMTExpr = expr match {
+    case e: BVOp => simplifyOp(e)
+    // TODO: maybe generalize by changing BVReduceAnd(x) => x == 1111 => split up concat
+    case BVReduceAnd(BVConcat(a, b)) if a.width == 1 && b.width == 1 => BVOp(Op.And, a, b)
+    case BVReduceOr(BVConcat(a, b)) if a.width == 1 && b.width == 1 => BVOp(Op.Or, a, b)
+    case BVExtend(e, 0, _) => e
+    case BVSlice(e, hi, 0) if hi == e.width - 1 => e
+    case BVNot(BVNot(e)) => e
+    case BVIte(condA, BVIte(condB, truB, falsB), falsA) if falsA == falsB => BVIte(and(condA, condB), truB, falsA)
+    case other => other
+  }
+
+  private def simplifyOp(expr: BVOp): BVExpr = expr match {
+    // TODO
+    //    case BVOp(Op.And, a, BVLiteral(1, 1)) => a
+    //    case BVOp(Op.And, BVLiteral(1, 1), b) => b
+    //    case BVOp(Op.And, _, BVLiteral(0, 1)) => BVLiteral(0, 1)
+    //    case BVOp(Op.And, BVLiteral(0, 1), _) => BVLiteral(0, 1)
+    //    case BVOp(Op.Or, _, BVLiteral(1, 1)) => BVLiteral(1, 1)
+    //    case BVOp(Op.Or, BVLiteral(1, 1), _) => BVLiteral(1, 1)
+    //    case BVOp(Op.Or, a, BVLiteral(0, 1)) => a
+    //    case BVOp(Op.Or, BVLiteral(0, 1), b) => b
+    case other => other
+  }
+
+  private def and(a: BVExpr, b: BVExpr): BVExpr = BVOp(Op.And, a, b)
 }
