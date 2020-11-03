009ff101baab8c7c0d0f992e626ff91a0af51780
Eashaan Katiyar <eashaank@gmail.com>
2020-11-02 18:36:23 -0800

    MVP Eval for Simple.btor

--- src/maltese/ExecutionEngine.scala
+++ src/maltese/ExecutionEngine.scala
@@ -1,6 +1,7 @@
 package maltese
 
-import maltese.smt.{BVLiteral, IsInit, Signal, TransitionSystem}
+import maltese.smt._
+import SMTExprEval._
 
 import scala.io.StdIn.readLine
 
@@ -11,23 +12,42 @@ class ExecutionEngine(val simplifiedSystem: TransitionSystem) {
     val inits = simplifiedSystem.signals.filter(_.lbl.equals(IsInit)).map(init => init.name -> init).toMap
     simplifiedSystem.states.map(state => state.sym.name -> initState(state.sym.name, inits))
   }
-  println(memoryMap)
+  private val constraints = simplifiedSystem.signals.filter(_.lbl.equals(IsConstraint))
+  private val badStates = simplifiedSystem.signals.filter(_.lbl.equals(IsBad))
+  private val transitions = simplifiedSystem.signals.filter(_.lbl.equals(IsNext))
 
-  def initState(state: String, inits: Map[String, Signal]): BigInt = {
-    if (inits.contains(state+".initi")) {
-      val expr = inits(state+".init").e
-      val ret = expr match {
-        case e: BVLiteral => e.value
-        case _ => printf("Can't assign state to %s\n", expr.getClass.getName)
-          inputPrompt()
-      }
-      return ret
-    }
-    inputPrompt()
+  def execute() = {
+    //prompt user for input
 
+    // check constraints
+
+    // execute transitions
+
+    // check bad states
   }
 
-  def inputPrompt(): BigInt = {
+  def eval(expr: SMTExpr): BigInt = {
+    expr match {
+      case e: BVLiteral => e.value
+      case e: BVSymbol => memoryMap(e.name)
+      case e: BVOp => doBVOp(e.op, eval(e.a), eval(e.b), e.width)
+      case e: BVExtend => doBVExtend(eval(e.e), e.width, e.by, e.signed)
+      case e: BVEqual => doBVEqual(eval(e.a), eval(e.b))
+      case e: BVNot => doBVNot(eval(e.e), e.width)
+      case e: BVComparison => doBVCompare(e.op, eval(e.a), eval(e.b), e.width, e.signed)
+      case _ => 10/0 //TODO: Add proper error
+    }
+  }
+
+  def initState(state: String, inits: Map[String, Signal]): BigInt = { // will init state always be BigInt?
+    if (inits.contains(state+".init")) {
+      val expr = inits(state+".init").e
+      return eval(expr)
+    }
+    inputPrompt()
+  }
+
+  def inputPrompt(): BigInt = { // how to best implement width-checking?
     print("Input a value: ")
     BigInt(readLine.toInt)
   }
