0fbb28033228151a6bea0e0f528d4f97a6c0d26d
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-04-28 12:29:48 -0700

    smt library is now open source

--- test/maltese/smt/solvers/Yices2Spec.scala
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 The Regents of the University of California
-// released under BSD 3-Clause License
-// author: Kevin Laeufer <laeufer@cs.berkeley.edu>
-
-package maltese.smt.solvers
-
-class Yices2Spec extends BasicSolverSpec(() => Yices2()) {
-
-}
