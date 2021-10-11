a17270cb39ac4182b7ea6d8604171d20a19f5c86
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-10-11 10:42:31 -0700

    add tiny rocket config without TL monitors

---
+++ benchmarks/chipyard/chipyard.TestHarness.TinyRocketConfig/sim_files.f
@@ -0,0 +1,15 @@
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.TinyRocketConfig/EICG_wrapper.v
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.TinyRocketConfig/emulator.cc
+-FI /home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.TinyRocketConfig/verilator.h
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.TinyRocketConfig/SimSerial.cc
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.TinyRocketConfig/testchip_tsi.cc
+-FI /home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.TinyRocketConfig/testchip_tsi.h
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.TinyRocketConfig/SimDRAM.cc
+-FI /home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.TinyRocketConfig/mm.h
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.TinyRocketConfig/mm.cc
+-FI /home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.TinyRocketConfig/mm_dramsim2.h
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.TinyRocketConfig/mm_dramsim2.cc
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.TinyRocketConfig/SimDTM.cc
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.TinyRocketConfig/SimJTAG.cc
+-FI /home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.TinyRocketConfig/remote_bitbang.h
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.TinyRocketConfig/remote_bitbang.cc
