2bac87242a420461db91e2c4aa68c1f9d7756276
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-10-22 12:14:43 -0700

    benchmarks: add rocket without tl monitor

--- benchmarks/chipyard/Readme.md
+++ benchmarks/chipyard/Readme.md
@@ -1,1 +1,21 @@
 Some chipyard designs generated with the `dev` branch (`05b309f97dbc87cbe24a257cd44f0f88962e09ce`) on 2021-10-07.
+
+
+```
+class KevinRocketConfig extends Config(
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new WithoutTLMonitors ++
+  new chipyard.config.AbstractConfig)
+
+class TinyRocketConfig extends Config(
+  new chipyard.config.WithTLSerialLocation(
+    freechips.rocketchip.subsystem.FBUS,
+    freechips.rocketchip.subsystem.PBUS) ++                       // attach TL serial adapter to f/p busses
+  new chipyard.WithMulticlockIncoherentBusTopology ++             // use incoherent bus topology
+  new freechips.rocketchip.subsystem.WithNBanks(0) ++             // remove L2$
+  new freechips.rocketchip.subsystem.WithNoMemPort ++             // remove backing memory
+  new freechips.rocketchip.subsystem.With1TinyCore ++             // single tiny rocket-core
+  new WithoutTLMonitors ++
+  new chipyard.config.AbstractConfig)
+
+```
---
+++ benchmarks/chipyard/chipyard.TestHarness.KevinRocketConfig/sim_files.f
@@ -0,0 +1,15 @@
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.KevinRocketConfig/EICG_wrapper.v
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.KevinRocketConfig/emulator.cc
+-FI /home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.KevinRocketConfig/verilator.h
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.KevinRocketConfig/SimSerial.cc
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.KevinRocketConfig/testchip_tsi.cc
+-FI /home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.KevinRocketConfig/testchip_tsi.h
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.KevinRocketConfig/SimDRAM.cc
+-FI /home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.KevinRocketConfig/mm.h
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.KevinRocketConfig/mm.cc
+-FI /home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.KevinRocketConfig/mm_dramsim2.h
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.KevinRocketConfig/mm_dramsim2.cc
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.KevinRocketConfig/SimDTM.cc
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.KevinRocketConfig/SimJTAG.cc
+-FI /home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.KevinRocketConfig/remote_bitbang.h
+/home/kevin/chipyard/sims/verilator/generated-src/chipyard.TestHarness.KevinRocketConfig/remote_bitbang.cc
