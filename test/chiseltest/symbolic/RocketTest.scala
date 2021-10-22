28774f08599a6abd3d373fc5f299658115defaeb
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-10-21 19:38:41 -0700

    wip: rocket test

--- test/chiseltest/symbolic/RocketTest.scala
+++ test/chiseltest/symbolic/RocketTest.scala
@@ -1,5 +1,7 @@
 package chiseltest.symbolic
 
+import chiseltest.simulator.{Compiler, SimulatorContext, VerilatorBackendAnnotation, WriteVcdAnnotation}
+import firrtl.{AnnotationSeq, CircuitState}
 import org.scalatest.flatspec.AnyFlatSpec
 
 class RocketTest extends AnyFlatSpec {
@@ -12,4 +14,28 @@ class RocketTest extends AnyFlatSpec {
     sim.inputs.foreach(sim.poke(_, 0))
     sim.reset()
   }
+}
+
+class ConcreteRocketTest extends FlatSpecWithTargetDir {
+  behavior of "RocketTile from Chipyard"
+
+  def loadFirrtl(src: String, annos: AnnotationSeq = List()): CircuitState = {
+    val state = CircuitState(firrtl.Parser.parse(src), annos)
+    Compiler.toLowFirrtl(state)
+  }
+
+  def load(src: String, annos: AnnotationSeq = List()): SimulatorContext = {
+    VerilatorBackendAnnotation.getSimulator.createContext(loadFirrtl(src, withTargetDir(annos)))
+  }
+
+  it should "execute in a simulation" in {
+    val filename = os.pwd / "benchmarks" / "chipyard" / "chipyard.TestHarness.TinyRocketConfig" / "RocketTile.opt.lo.fir"
+    val sim = load(os.read(filename), Seq(WriteVcdAnnotation))
+    sim.poke("reset", 1)
+    sim.step()
+    sim.poke("reset", 0)
+    sim.step(5)
+    sim.finish()
+  }
+
 }
